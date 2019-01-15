59bc29e4c4f20418adf53922976869b224adc654
TheMynx <camp.rock72@gmail.com>
2019-01-15 23:40:02 +0100

    New feature - histogram showing deck curve for the !deck command. Requires matplotlib, install with pip

--- card_bot.py
+++ card_bot.py
@@ -8,6 +8,7 @@ import Levenshtein
 import discord
 import asyncio
 import os
+import matplotlib.pyplot as plt
 
 with open("deckCommandData/cardIDs.json") as f:
     cardIDs = json.load(f)
@@ -79,6 +80,9 @@ async def card(ctx, *, c: str):
 @commands.cooldown(1,4,BucketType.channel)
 @bot.command()
 async def deck(ctx, *, code:str):
+    barX = []
+    barY = []
+    amountOfCardsPerMagicka = {}
     splitCode = findall("..", code) # Splits the input string into a list of 2 characters at a time
     if splitCode[0] != "SP":
         print("Not a valid deck code.")
@@ -91,7 +95,6 @@ async def deck(ctx, *, code:str):
         # Finding cards in deck
         count = 0
         deckCardsSortedByCost = {}
-
         for i in splitCode:
             if i in IDs:
                 count+=1
@@ -106,35 +109,80 @@ async def deck(ctx, *, code:str):
         # Checking amount of cards in deck
         cardCount = 0
         for i in deckCardsSortedByCost:
+
             for x in deckCardsSortedByCost[i]:
                 if "1x" in x:
                     cardCount += 1
+                    try:
+                        amountOfCardsPerMagicka[i] += 1
+                    except KeyError:
+                        amountOfCardsPerMagicka[i] = 1
                 elif "2x" in x:
                     cardCount += 2
+                    try:
+                        amountOfCardsPerMagicka[i] += 2
+                    except KeyError:
+                        amountOfCardsPerMagicka[i] = 2
                 else:
                     cardCount += 3
+                    try:
+                        amountOfCardsPerMagicka[i] += 3
+                    except KeyError:
+                        amountOfCardsPerMagicka[i] = 3
+        for i in amountOfCardsPerMagicka:
+            barX.append(i)
+            barY.append(amountOfCardsPerMagicka[i])
+        dictionaryBarXBarY = dict(zip(barX, barY))
+        for i in range(min(barX), max(barX)+1):
+            try:
+                dictionaryBarXBarY[i] += 0
+            except KeyError:
+                dictionaryBarXBarY[i] = 0
+        dictionaryBarXBarY = SortedDict(dictionaryBarXBarY)
+        barX = list(dictionaryBarXBarY.keys())
+        barY = list(dictionaryBarXBarY.values())
+        lastItem = 0
+        if len(barX) > 8 and barX[0] == 0:
+            while len(barX) > 8:
+                barX.pop()
+            while len(barY) > 8:
+                lastItem = barY[-1]
+                barY.pop()
+                barY[-1] = barY[-1] + lastItem
+        elif len(barX) > 7 and barX[0] == 1:
+            while len(barX) > 7:
+                barX.pop()
+            while len(barY) > 7:
+                lastItem = barY[-1]
+                barY.pop()
+                barY[-1] = barY[-1] + lastItem
+        plt.hist([])
+        plt.xlim(min(barX) -0.5, max(barX) +0.5)
+        plt.bar(barX, barY)
+        plt.xticks(barX)
+        plt.yticks(range(0, max(barY), 2))
+        plt.title("Magicka Curve")
+        plt.xlabel("Magicka")
+        plt.ylabel("Card Count")
+        plt.draw()
+        labels = plt.gca().get_xticks().tolist()
+        labels[6] = "7+"
+        plt.gca().set_xticklabels(labels)
+        plt.savefig("curve.png")
         # Embed message
+        f = discord.File("curve.png")
         embed=discord.Embed(title="Deck Code: " + code, description="Card Count: " + str(cardCount), color=0xdf0000)
         embed.set_author(name=ctx.message.author, icon_url=ctx.message.author.avatar_url)
         for i in deckCardsSortedByCost:
                 deckCardsSortedByCost[i] = "".join(deckCardsSortedByCost[i])
                 embed.add_field(name=str(i) + " Magicka", value=deckCardsSortedByCost[i], inline=True)
+        embed.set_thumbnail(url="attachment://curve.png")
         try:
             await ctx.message.delete()
-            await ctx.send(embed=embed)
+            await ctx.send(file=f, embed=embed)
         except Forbidden:
             await ctx.send("The bot does not have 'Manage messages' or 'Embed links' permissions.")
 
-@commands.cooldown(1,4,BucketType.channel)
-@bot.command()
-async def github(ctx):
-    embed=discord.Embed(title="Open source code (click on me)", url="https://github.com/TheMynx/open-source-tesl-bot", color=0xdf0000)
-    embed.set_author(name=ctx.message.author, icon_url=ctx.message.author.avatar_url)
-    try:
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
-    except Forbidden:
-        await ctx.send("The bot does not have 'Manage messages' or 'Embed links' permissions.")
 
 @commands.cooldown(1,4,BucketType.channel)
 @bot.command()
